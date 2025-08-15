1c2868638b24be31e11630fc8bdaef498b66080a
jod35 <jodestrevin@gmail.com>
2025-08-15 15:28:39 +0300

    edit setup.sh, modify how tosetup configs

--- setup.sh
+++ setup.sh
@@ -1,18 +1,46 @@
 
 #!/bin/bash
 
-echo "####################################################\n"
-echo "###### Post Install script for i3 on jod35' machine ############"
-echo "#####################################################\n"
-echo "Installing the status bar"
-# mv bumblebee-status .bumblebee
-echo "#####################################################\n"
-echo "Installing the fonts and icons"
-git clone https://github.com/gabrielelana/awesome-terminal-fonts
+# move to home directory
 
+cd ~
 
+echo "Copying configuration files to ~/.config..."
+# Create directories if they don't exist
 
+mkdir -p .config/polybar
+mkdir -p .config/i3
+mkdir -p .config/terminator
+mkdir -p .config/rofi
+mkdir -p .config/picom
 
+# Copy configuration files
+cp -r polybar/config .config/polybar/
+cp -r i3/config .config/i3/
+cp -r terminator/config .config/terminator/
+cp -r rofi/config.rasi .config/rofi/
+cp -r picom/picom.conf .config/picom/
 
+echo "Configuration files copied successfully."
 
+echo "setting up Bumblebee status..."
 
+# install git if not installed
+if ! command -v git &> /dev/null; then
+    echo "git could not be found, installing..."
+    sudo apt-get update
+    sudo apt-get install -y git
+else
+    echo "git is already installed."
+fi
+
+# Clone the Bumblebee status repository
+if [ ! -d "$HOME/.bumblebee" ]; then
+    echo "Cloning Bumblebee status repository..."
+    git clone https://github.com/tobi-wan-kenobi/bumblebee-status.git
+else
+    echo "Bumblebee status repository already exists."
+fi
+
+# rename bumblebee-status to .bumblebee
+mv bumblebee-status "$HOME/.bumblebee"
