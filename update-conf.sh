665d62749b01bdd0d457385a3bd2209d1370dd51
jod35 <jodestrevin@gmail.com>
2025-09-08 12:07:58 +0300

    update tokyo night

---
+++ update-conf.sh
@@ -0,0 +1,13 @@
+
+echo "Updating i3 config ..."
+cp -r ~/.config/i3/config .
+
+echo "Terminator Config ..."
+cp -r ~/.config/terminator/config .
+
+echo "picom config ..."
+cp -r ~/.config/picom/picom.conf . 
+
+echo "rofi config ..."
+cp -r ~/.config/rofi/config.rasi .
+
