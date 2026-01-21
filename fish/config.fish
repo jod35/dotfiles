8bc84965467fbb7a55a80909a7a5df4864e69365
jod35 <jodestrevin@gmail.com>
2026-01-21 22:30:54 +0300

    update conf

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,15 @@ if status is-interactive
    fastfetch
    alias cls="clear"
    alias ag="~/Antigravity/antigravity &"
-   alias code="code-oss"
+   alias code="~/code/code"
+   alias node="/home/jod35/.nvm/versions/node/v24.12.0/bin/node"
+   alias npm="/home/jod35/.nvm/versions/node/v24.12.0/bin/npm"
+   
 end
+
+# pnpm
+set -gx PNPM_HOME "/home/jod35/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
